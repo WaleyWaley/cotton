c76a7143cd59be08cefed4dff5c20b9ad5af3b4b
LapTop <h18381603535@gmail.com>
2026-01-19 21:17:33 +0800

    完善AsyncLogger.h

--- include/AsyncLogger.h
+++ include/AsyncLogger.h
@@ -2,9 +2,183 @@
 
 #include "EventFixedBuffer.hpp"
 #include "logger/Logger.h"
+#include "common/alias.h"
 #include <algorithm>
 #include <vector>
 #include <condition_variable>
 #include <print>
 #include <latch>
 
+class AsyncLogger : public Logger {
+public:
+    // 也可以写成 using EventBuffer    = EventFixedBuffer<>; 因为模板设置了默认值
+    using EventBuffer    = EventFixedBuffer<c_k_event_count>;
+    // 同一时刻只准一个进程写入Buffer
+    using EventBufferPtr = std::unique_ptr<EventBuffer>;
+
+public:
+    // 构造函数：初始化缓冲区和日志文件，启动日志线程
+    explicit AsyncLogger(int flush_interval = 3) // 3秒刷新一次
+        : flush_interval_ {flush_interval}
+        , running_(false)
+        , current_buffer_(std::make_unique<EventFixedBuffer<>>()) // 初始化双缓冲
+        , next_buffer_(std::make_unique<EventFixedBuffer<>>())
+    {
+        // 初始化备用缓冲列表，用于收集应用线程写满的缓冲
+        buffers_to_write_.reserve(16);
+    }
+
+    AsyncLogger(const AsyncLogger&)            = delete;
+    AsyncLogger(AsyncLogger&&)                 = delete;
+    AsyncLogger& operator=(const AsyncLogger&) = delete;
+    AsyncLogger& operator=(AsyncLogger&&)      = delete;
+
+    ~AsyncLogger()
+    {
+        if (running_)
+        {
+            stop();
+        }
+    }
+
+    // 核心接口：供应用线程调用，只接收 LogEvent
+    auto append(LogEvent event) -> void
+    {
+        bool should_notify = false; // 标记是否需要通知
+
+        {
+            auto _ = std::lock_guard<std::mutex> {mutex_};
+            // 尝试写入当前缓冲区（检查事件数量）
+            if (current_buffer_->available() > 0)
+            {
+                current_buffer_->append(std::move(event));
+            }
+            else
+            {
+                // 防止内存爆掉，直接丢弃当前日志
+                if(buffers_to_write_.size() > 25)
+                {
+                    std::cerr << "Too much events to write to buffers (buffers_to_write's size > 25)" << std::endl;
+                    return;
+                }
+                //  缓冲区已满
+                buffers_to_write_.push_back(std::move(current_buffer_));
+                // 交换缓冲区逻辑
+                if (next_buffer_)
+                {
+                    // 如果有"备胎"，之间转正
+                    current_buffer_ = std::move(next_buffer_);
+                }
+                else
+                {
+                    // 如果没有备胎，只能现造一个
+                    current_buffer_ = std::make_unique<EventBuffer>();
+                }
+                current_buffer_->append(std::move(event)); // 写入新的 current_buffer_
+
+                // 标记需要通知
+                should_notify = true;
+            }
+        } // 锁结束
+
+        // 锁外通知，避免惊群效应
+        // 锁释放后再通知，后端线程醒来就能立马拿到锁
+        if(should_notify)
+        {
+            cond_.notify_one();
+        }
+    }
+
+    // 启动日志线程
+    auto start() -> void
+    {
+        running_ = true;
+        thread_ = std::thread(&AsyncLogger::threadFunc_, this);
+        latch_.wait();
+    }
+
+    // 停止日志线程
+    auto stop() -> void
+    {
+        running_ = false;
+        cond_.notify_one(); //唤醒线程使其退出循环
+        if(thread_.joinable())
+        {
+            thread_.join();
+        }
+    }
+        
+private:
+    // 后台日志线程执行的函数(消费者)
+    auto threadFunc_() -> void
+    {
+        latch_.count_down();
+        // 预分配用于交换的缓冲区，避免在日志线程中频繁分配内存
+        auto new_buffer1 = std::make_unique<EventBuffer>();
+        auto new_buffer2 = std::make_unique<EventBuffer>();
+        // 用于处理待写入的缓冲区
+        auto buffers_to_process = std::vector<EventBufferPtr>{};
+        buffers_to_process.reserve(16); // 存放16个缓冲区
+
+        while(running_)
+        {
+            {  
+                // 等待条件变量唤醒或者超时
+                auto lock = std::unique_lock<std::mutex>(mutex_);
+
+                // 等待: 知道超时 (flush_interval_) 或被通知有数据
+                if(buffers_to_write_.empty())
+                {
+                    cond_.wait_for(lock, std::chrono::seconds(flush_interval_));
+                }
+
+                // 1. 将 current_buffer_ 移入待处理列表 (即使未满，也到时间刷新了)
+                buffers_to_write_.push_back(std::move(current_buffer_));
+                // 2. 将新分配的 new_buffer1 替换为新的 current_buffer_
+                current_buffer_ = std::move(new_buffer1);
+                // 3. 交换待写入列表：将应用线程的数据移交给日志线程
+                buffers_to_process.swap(buffers_to_write_);
+                // 此时，应用线程可以继续向 buffers_to_write_ 写入，互不影响
+
+                // 4. 将空的 new_buffer2 替换为新的 next_buffer_
+                if(next_buffer_ == nullptr)
+                {
+                    next_buffer_ = std::move(new_buffer2);
+                }
+            }   // 互斥锁释放
+
+            // --- 日志线程开始 I/O 操作 (无锁) ---
+            if(buffers_to_process.size() > 25)
+            {
+                // 如果待处理缓冲区过多，说明生产速度远超消费速度，可能出现日志堆积
+                // 这里简单丢弃部分日志以防内存占用过高
+                auto now = std::chrono::time_point_cast<Seconds>(SystemClock::now());
+                auto zoned_time = ZoneTime<Seconds>{
+                    std::chrono::current_zone(), now
+                };
+                auto time_point_str = std::format("{:%Y-%m-%d-%H-%M-%S}", zoned_time.get_local_time());
+                std::println("Dropper log messages at {}, {} larger than buffer", time_point_str, buffers_to_process.size()-2);
+                // 直接丢掉多余的日志
+                buffers_to_process.erase(buffers_to_process.begin()+2,buffers_to_process.end());
+            }
+            // 5. 将所有缓冲区内容写入文件
+            
+        }
+    }
+
+private:
+    // 配置
+    const int flush_interval_; // 强制刷新间隔（秒）
+
+    // 线程和同步
+    std::thread thread_;
+    std::atomic<bool> running_;
+    std::mutex mutex_;
+    std::condition_variable cond_;
+    std::latch latch_{1};
+
+    // 双缓冲机制的核心
+    EventBufferPtr current_buffer_;                // 当前应用线程正在写入的缓冲区
+    EventBufferPtr next_buffer_;                   // 备用缓冲区（用于减少应用线程等待时间）
+    std::vector<EventBufferPtr> buffers_to_write_; // 已写满，等待后台线程写入的缓冲区列表
+};
